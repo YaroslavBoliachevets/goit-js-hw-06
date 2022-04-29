6be0b99fa98d21d5c3c59a6b02c0183360d2e63d
YaroslavBoliachevets <boliachevets.ya@gmail.com>
2022-04-30 00:16:00 +0300

    Перебирающий метод массива в 1 задании

--- js/task-01.js
+++ js/task-01.js
@@ -4,8 +4,15 @@ const categoryNameEl = document.querySelectorAll('#categories h2');
 
 console.log('Number of categories:', categoriesEl);
 
-for (const el of categoryNameEl) {
-  console.log('Category: ', el.textContent);
+// for (const el of categoryNameEl) {
+//   console.log('Category: ', el.textContent);
 
+//   console.log('Elements: ', el.nextElementSibling.childElementCount);
+// }
+
+categoryNameEl.forEach(mainContent);
+
+function mainContent(el) {
+  console.log('Category: ', el.textContent);
   console.log('Elements: ', el.nextElementSibling.childElementCount);
 }
